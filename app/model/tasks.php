36dea13cc382ef35e046e5835bc90bb461e41aa2
gfloriano11 <gflorianodev@gmail.com>
2025-03-20 17:46:41 -0300

    feat: get tasks functio

--- app/model/tasks.php
+++ app/model/tasks.php
@@ -1,2 +1,31 @@
 <?php
+    class Tasks{
 
+
+        public static function getTasks(){
+
+            $conn = Connection::getConn();
+
+            $query = "SELECT * FROM tasks";
+
+            $statement = $conn->prepare($query);
+
+            $statement->execute();
+
+            $result = $statement->get_result();
+
+            while($row = $result->fetch_assoc()){
+                $data = $row; 
+            }
+
+            if(!$data){
+                $data = 'Não há tarefas';
+            }
+
+            var_dump($data);
+
+            return $data;
+        }
+    }
+
+?>
