e9ca9cb97486beec54bf6f444ec1c64a48d93928
gfloriano11 <gflorianodev@gmail.com>
2025-03-20 17:16:20 -0300

    feat: create sql script

---
+++ app/assets/script.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS to_do_list;
+
+CREATE DATABASE to_do_list;
+
+USE to_do_list;
+
+CREATE TABLE tasks (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    task_name VARCHAR(255) NOT NULL,
+    task_desc TEXT,
+    task_status ENUM('criada', 'em andamento', 'finalizada') DEFAULT ('criada'),
+    created_at DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
