348c5c1b619979d6f7f0cfdc1faa06f14767e14b
gfloriano11 <gflorianodev@gmail.com>
2025-03-20 21:10:05 -0300

    feat: create for each to pull tasks

--- src/script/tasks.js
+++ src/script/tasks.js
@@ -1,23 +1,34 @@
 async function getTasks(){
-
+    
+    let tasks;
+    
     try{ 
         const response = await fetch('../app/index.php?method=tasks', {
             method: 'GET',
             headers: {
-              'Content-Type': 'application/json'
+                'Content-Type': 'application/json'
             }
-          });
-
+        });
+        
         if(!response.ok){
             throw new Error(`Erro na requisição: ${response.status} ${response.statusText}`);
         }
-
-        const data = await response.json();
-        console.log(data);
-
+        
+        tasks = await response.json();
+        
     } catch (error){
         console.log('Erro ao buscar tasks', error);
     }
+    
+    tasks.forEach(task => {
+        const taskContainer = document.createElement('div');
+        const titleContainer = document.createElement('div');
+        const taskTitle = document.createElement('p');
+
+        taskTitle.innerText = task.task_name;
+    })
+    
+
 }
 
 getTasks()
