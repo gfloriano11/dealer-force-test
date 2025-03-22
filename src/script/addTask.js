38ad5bd893aecbaa76911a59e90f1fe7a06a07e9
gfloriano11 <gflorianodev@gmail.com>
2025-03-22 14:37:30 -0300

    feat: function to open and close form

---
+++ src/script/addTask.js
@@ -0,0 +1,27 @@
+const addTaskButton = document.querySelector('#add_task');
+const cancelButton = document.querySelector('.cancel_button');
+
+addTaskButton.addEventListener('click', () => {
+
+    const tasks = document.querySelector('#tasks');
+    const title = document.querySelector('#tasks_title');
+
+    const addTaskForm = document.querySelector('#add_task_form');
+
+    tasks.classList.add('blur');
+    title.classList.add('blur');
+    addTaskButton.classList.add('blur');
+    addTaskForm.classList.remove('hide');
+});
+
+cancelButton.addEventListener('click', () => {
+    const tasks = document.querySelector('#tasks');
+    const title = document.querySelector('#tasks_title');
+
+    const addTaskForm = document.querySelector('#add_task_form');
+
+    tasks.classList.remove('blur');
+    title.classList.remove('blur');
+    addTaskButton.classList.remove('blur');
+    addTaskForm.classList.add('hide');
+})
