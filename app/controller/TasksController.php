371c80124ab9ee844385c1fec7148ebf0ad766f2
gfloriano11 <gflorianodev@gmail.com>
2025-03-23 20:36:52 -0300

    feat: task controller calls create task

--- app/controller/TasksController.php
+++ app/controller/TasksController.php
@@ -2,11 +2,20 @@
 
     class TasksController{
         
-        public function tasks(){
+        public function getTasks(){
 
             $tasks = Tasks::getTasks();
 
             echo json_encode($tasks);
 
+        }
+
+        public function createTask(){
+
+            $task_info = json_decode(file_get_contents('php://input'), true);
+
+            $task = Tasks::createTask($task_info);
+
+            echo json_encode($task);
         }
     }
