c783fe9c04929bf61cb7ba8a38e1b2207b93cda4
gfloriano11 <gflorianodev@gmail.com>
2025-03-24 16:56:38 -0300

    fix: update task same file than get tasks

--- src/script/tasks.js
+++ src/script/tasks.js
@@ -30,6 +30,8 @@ async function getTasks(){
         const statusContainer = document.createElement('div');
         const checkContainer = document.createElement('div');
         const checkIcon = document.createElement('img');
+        const editContainer = document.createElement('div');
+        const deleteContainer = document.createElement('div');
         const editIcon = document.createElement('img');
         const trashIcon = document.createElement('img');
         const taskInfo = document.createElement('div');
@@ -40,13 +42,9 @@ async function getTasks(){
         const finalDate = document.createElement('p');
         const taskStatus = document.createElement('p');
 
-        console.log(task.final_date);
-
         const [ano, mes, dia] = task.final_date.split("-");
         const formattedDate = `${dia}/${mes}/${ano}`;
-        console.log(formattedDate);
         
-
         taskTitle.innerText = task.task_name;
         taskDesc.innerText = task.task_desc;
         finalDate.innerText = formattedDate;
@@ -56,8 +54,10 @@ async function getTasks(){
         taskContainer.appendChild(checkContainer);
         taskContainer.setAttribute('task_id', task.id);
         checkContainer.appendChild(checkIcon);
-        actionsContainer.appendChild(editIcon);
-        actionsContainer.appendChild(trashIcon);
+        actionsContainer.appendChild(editContainer);
+        actionsContainer.appendChild(deleteContainer);
+        editContainer.appendChild(editIcon);
+        deleteContainer.appendChild(trashIcon);
         checkIcon.setAttribute('src', 'assets/check-mark.svg');
         editIcon.setAttribute('src', 'assets/line-md--edit.svg');
         trashIcon.setAttribute('src', 'assets/material-symbols--delete-outline.svg');
@@ -83,14 +83,56 @@ async function getTasks(){
         taskContainer.classList.add('task');
         checkContainer.classList.add('check');
         checkIcon.classList.add('check_icon');
-        editIcon.classList.add('icon');
-        editIcon.classList.add('edit_icon');
-        trashIcon.classList.add('icon');
+        editContainer.classList.add('icon');
+        editContainer.classList.add('edit_icon');
+        deleteContainer.classList.add('icon');
         actionsContainer.classList.add('actions')
         titleContainer.classList.add('task_title');
         descContainer.classList.add('task_desc');
         dateContainer.classList.add('created_at');
         statusContainer.classList.add('task_status');
+
+        const addTaskButtonEdit = document.querySelector('#add_task');
+        const titleEdit = document.querySelector('#tasks_title');
+        const cancelEdit = document.querySelector('.cancel_edit');
+        const editForm = document.querySelector('#edit_task_form')
+        const editButton = document.querySelector('.edit_task')
+
+        editContainer.addEventListener('click', () => {
+            
+            editForm.classList.remove('hide');
+            titleEdit.classList.add('blur');
+            addTaskButtonEdit.classList.add('blur');
+
+        })
+
+        editButton.addEventListener('click', async () => {
+
+            const response = await fetch('http://localhost:80/dealer-force-test/app/index.php?method=tasksk&action=edit', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    taskId: task.id,
+                    taskName: task.task_name,
+                    taskDesc: task.task_desc,
+                    taskDate: task.final_date
+                })
+            })
+        
+            if(!response.ok){
+                throw new Error('Não foi possível editar task');
+            }
+        })
+
+        cancelEdit.addEventListener('click', () => {
+            editForm.classList.add('hide');
+            titleEdit.classList.remove('blur');
+            addTaskButtonEdit.classList.remove('blur');
+        })
+
+
     })
     
 
