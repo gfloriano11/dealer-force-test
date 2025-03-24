d8ff3122441111c6e3039a80c8be4da527025459
gfloriano11 <gflorianodev@gmail.com>
2025-03-23 22:53:56 -0300

    feat: create tasks

--- src/script/tasks.js
+++ src/script/tasks.js
@@ -21,16 +21,67 @@ async function getTasks(){
     }
     
     tasks.forEach(task => {
+
         const tasksContainer = document.querySelector('#tasks');
         const taskContainer = document.createElement('div');
         const titleContainer = document.createElement('div');
+        const descContainer = document.createElement('div');
+        const dateContainer = document.createElement('div');
+        const statusContainer = document.createElement('div');
+        const checkContainer = document.createElement('div');
+        const checkIcon = document.createElement('img');
+        const editIcon = document.createElement('img');
+        const trashIcon = document.createElement('img');
+        const taskInfo = document.createElement('div');
+        const actionsContainer = document.createElement('div');
+        const otherInfo = document.createElement('div');
         const taskTitle = document.createElement('p');
+        const taskDesc = document.createElement('p');
+        const finalDate = document.createElement('p');
+        const taskStatus = document.createElement('p');
 
         taskTitle.innerText = task.task_name;
+        taskDesc.innerText = task.task_desc;
+        finalDate.innerText = task.final_date;
+        taskStatus.innerText = task.task_status;
 
         tasksContainer.appendChild(taskContainer);
+        taskContainer.appendChild(checkContainer);
+        checkContainer.appendChild(checkIcon);
+        actionsContainer.appendChild(editIcon);
+        actionsContainer.appendChild(trashIcon);
+        checkIcon.setAttribute('src', 'assets/check-mark.svg');
+        editIcon.setAttribute('src', 'assets/line-md--edit.svg');
+        trashIcon.setAttribute('src', 'assets/material-symbols--delete-outline.svg');
         taskContainer.appendChild(titleContainer);
+        taskContainer.appendChild(descContainer);
+        taskContainer.appendChild(taskInfo);
+        taskContainer.appendChild(actionsContainer);
+        taskContainer.appendChild(otherInfo);
+        otherInfo.appendChild(dateContainer);
+        otherInfo.appendChild(taskStatus);
+
+        taskInfo.appendChild(titleContainer);
+        taskInfo.appendChild(descContainer);
+        taskInfo.appendChild(dateContainer);
+        taskInfo.appendChild(statusContainer);
+
         titleContainer.appendChild(taskTitle);
+        descContainer.appendChild(taskDesc);
+        dateContainer.appendChild(finalDate);
+        statusContainer.appendChild(taskStatus);
+
+        taskInfo.classList.add('task_info')
+        taskContainer.classList.add('task');
+        checkContainer.classList.add('check');
+        checkIcon.classList.add('check_icon');
+        editIcon.classList.add('icon');
+        trashIcon.classList.add('icon');
+        actionsContainer.classList.add('actions')
+        titleContainer.classList.add('task_title');
+        descContainer.classList.add('task_desc');
+        dateContainer.classList.add('created_at');
+        statusContainer.classList.add('task_status');
     })
     
 
